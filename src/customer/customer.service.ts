b138bfb7cad2a943ab95f7df1fdc93ba8b249a31
nemanjapapucic <97614697+nemanjapapucic@users.noreply.github.com>
2023-05-12 17:15:02 +0200

    small update

--- src/customer/customer.service.ts
+++ src/customer/customer.service.ts
@@ -3,13 +3,14 @@ import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { Customer } from './schemas/customer.schema';
 import { CustomerResponse } from './dto/customer.response';
+import { retry } from 'rxjs';
 
 @Injectable()
 export class CustomerService {
   constructor(
     @InjectModel(Customer.name)
     private customerModel: mongoose.Model<Customer>,
-  ) {}
+  ) { }
 
   async createCustomer(customer: Customer): Promise<Customer> {
     return await this.customerModel.create(customer);
@@ -36,13 +37,22 @@ export class CustomerService {
   }
 
   async updateById(id: string, customer: Customer): Promise<Customer> {
-    return await this.customerModel.findByIdAndUpdate(id, customer, {
+    let update_customer = this.customerModel.findByIdAndUpdate(id, customer, {
       new: true,
       runValidators: true,
     });
+    if (!update_customer) {
+      throw new Error('Customer not found.');
+    }
+    return update_customer
+
   }
 
   async deleteById(id: string): Promise<Customer> {
-    return await this.customerModel.findByIdAndDelete(id);
+    let customer = await this.customerModel.findByIdAndDelete(id);
+    if (!customer) {
+      throw new Error('Customer not found.');
+    }
+    return customer
   }
 }
